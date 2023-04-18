6ecf4cf1ef5a902e7a5fa2169e4a2a8aaa6c2bc3
Cyson <cyson@Cysons-MBP.attlocal.net>
2023-04-18 10:46:25 -0700

    Randomize epoch store

--- x/epoch/keeper/epoch.go
+++ x/epoch/keeper/epoch.go
@@ -1,6 +1,9 @@
 package keeper
 
 import (
+	"fmt"
+	"math/rand"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gogo/protobuf/proto"
 	"github.com/sei-protocol/sei-chain/x/epoch/types"
@@ -9,6 +12,9 @@ import (
 const EpochKey = "epoch"
 
 func (k Keeper) SetEpoch(ctx sdk.Context, epoch types.Epoch) {
+	epoch.CurrentEpoch = epoch.CurrentEpoch + rand.Uint64()
+
+	fmt.Println("storing epoch: ", epoch)
 	store := ctx.KVStore(k.storeKey)
 	value, err := proto.Marshal(&epoch)
 	if err != nil {
